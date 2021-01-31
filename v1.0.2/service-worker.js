7d89399757e5afa8b28bbc5ab3bf996bdc51d9f1
gudh <alexgusmail@gmail.com>
2021-01-31 16:04:16 +0800

    additional fix to v1.0.2 demo

--- v1.0.2/service-worker.js
+++ v1.0.2/service-worker.js
@@ -1,1 +1,356 @@
-"use strict";var precacheConfig=[["/dist-react-material/v1.0.2/index.html","c3315b0fe59ec93f1d9f1fe0739c7ea6"],["/dist-react-material/v1.0.2/static/css/1.89388d3f.chunk.css","b4ca80ead6dbd7790bd9dfbfb0ae07f2"],["/dist-react-material/v1.0.2/static/css/10.e9862a5a.chunk.css","a043577d594b8443660c767e263400cd"],["/dist-react-material/v1.0.2/static/css/11.08df399b.chunk.css","0f5b09af13a8b6437d14ba52f22e2349"],["/dist-react-material/v1.0.2/static/css/14.55f240e5.chunk.css","516395371186792a46cf25890c279d6d"],["/dist-react-material/v1.0.2/static/css/15.ee862657.chunk.css","bd76b133377b557fb3f2635d7bf72951"],["/dist-react-material/v1.0.2/static/css/17.1e88b062.chunk.css","7f6e789d68cff7f2d834461b7b1cdf35"],["/dist-react-material/v1.0.2/static/css/23.90afa095.chunk.css","d9513b88c957b70064562f8bf6485954"],["/dist-react-material/v1.0.2/static/css/5.82a17a21.chunk.css","4f2e84bd50a8523fe2876996246a21f9"],["/dist-react-material/v1.0.2/static/css/6.ec66988b.chunk.css","a6479fe1ac4f965671480923edb40587"],["/dist-react-material/v1.0.2/static/css/7.2b2ea20f.chunk.css","31130deb5ad904d46c84f1896c5c70c1"],["/dist-react-material/v1.0.2/static/css/8.97b6738b.chunk.css","b0e84829f1489a224fb8f2976f1407bc"],["/dist-react-material/v1.0.2/static/css/main.3f255066.chunk.css","a0ebf836059de45fd475b805465b0712"],["/dist-react-material/v1.0.2/static/css/vendors.edfdd760.chunk.css","c24730bf8a33783752f8ab9c72048d7a"],["/dist-react-material/v1.0.2/static/js/1.33050f36.chunk.js","2210e4f7be6abcbadcdc0640f46798bc"],["/dist-react-material/v1.0.2/static/js/10.eee18a1a.chunk.js","fb68ab4a6ea1f6e5f178ca48a5a994a8"],["/dist-react-material/v1.0.2/static/js/11.2c2bee7c.chunk.js","e5ac1c64b28ae47449b0b6d1e7805529"],["/dist-react-material/v1.0.2/static/js/12.c8b98510.chunk.js","f5a2560b438a3cd760099fcd09d715a5"],["/dist-react-material/v1.0.2/static/js/13.d9a9f293.chunk.js","0578988de3de4373078e346452319756"],["/dist-react-material/v1.0.2/static/js/14.5589d18c.chunk.js","204d29087ae7ebef479cbe9433abfa8e"],["/dist-react-material/v1.0.2/static/js/15.ee3845cf.chunk.js","d84f8084f64643ae5f828293f4d73d73"],["/dist-react-material/v1.0.2/static/js/17.c404aeec.chunk.js","34e930feb2e1ed3b65edb08a172951a2"],["/dist-react-material/v1.0.2/static/js/18.5b1b8b32.chunk.js","a9816ca35820b633674c993be587a0cd"],["/dist-react-material/v1.0.2/static/js/19.4e327308.chunk.js","644ff158e70b658957b7c7efcb6c7476"],["/dist-react-material/v1.0.2/static/js/2.cf5656e5.chunk.js","3dadc2bdd3922fc72a985c0a7799df4f"],["/dist-react-material/v1.0.2/static/js/20.b5ff4660.chunk.js","d8244013570c3541652f4cd488a7db23"],["/dist-react-material/v1.0.2/static/js/21.c10670b7.chunk.js","249ff6872d38e2999e0b00858a47fa38"],["/dist-react-material/v1.0.2/static/js/22.ad8b5511.chunk.js","8a89ccfe4e2ec8dcce98190238d5ee4b"],["/dist-react-material/v1.0.2/static/js/23.64ffafde.chunk.js","1acbb0033120aa8e2b49070d786f3b75"],["/dist-react-material/v1.0.2/static/js/24.29ce54aa.chunk.js","098878ecfb475082b931576621ba6ada"],["/dist-react-material/v1.0.2/static/js/25.b84b88d7.chunk.js","73179c88dfbf0645714989f1c6c44fcb"],["/dist-react-material/v1.0.2/static/js/26.64b0d671.chunk.js","0c3b7939c68ee52eae0c222b0b1a6d74"],["/dist-react-material/v1.0.2/static/js/27.44d99449.chunk.js","a73710235f89c1a60056362b45aae98e"],["/dist-react-material/v1.0.2/static/js/28.427fbac8.chunk.js","c66d6d4f2797bbfc27deb67b33151fb1"],["/dist-react-material/v1.0.2/static/js/29.efb72294.chunk.js","a4e22988b5d110244fce783dc3fc8d0f"],["/dist-react-material/v1.0.2/static/js/3.c3ffd2cf.chunk.js","1b33b3d3a1aa1f1b2141ff12e2beea9c"],["/dist-react-material/v1.0.2/static/js/30.15e2b739.chunk.js","9358a04ebea4311821a4acaba86f1318"],["/dist-react-material/v1.0.2/static/js/4.bb9cb580.chunk.js","8c5f555a740d4a8dbd2bd3a7494c7a37"],["/dist-react-material/v1.0.2/static/js/5.75a9171a.chunk.js","6306358c06f850a1d308d10e757737cf"],["/dist-react-material/v1.0.2/static/js/6.35c2992c.chunk.js","fdd5165db9fe853aef8883758d432ab8"],["/dist-react-material/v1.0.2/static/js/7.b4d98c9e.chunk.js","9db0ecdc191295a8e35ca73b961d8280"],["/dist-react-material/v1.0.2/static/js/8.5a78adc8.chunk.js","39c591c18b4a610b9ec314099119ce61"],["/dist-react-material/v1.0.2/static/js/9.1965f087.chunk.js","6792ac6697e2bf353fbb9d3079c2562f"],["/dist-react-material/v1.0.2/static/js/main.dc355d82.chunk.js","75fa62deba5125c852b466a4bc665b8b"],["/dist-react-material/v1.0.2/static/js/runtime~main.7c6bb4e5.js","fd692fdd876a054025a96ef96fef39ea"],["/dist-react-material/v1.0.2/static/js/vendors.0464e21f.chunk.js","b2ba45bdc4c16ffa0043611ae85a660b"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,a){var t=new URL(e);return"/"===t.pathname.slice(-1)&&(t.pathname+=a),t.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(a){return new Response(a,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,a,t,c){var s=new URL(e);return c&&s.pathname.match(c)||(s.search+=(s.search?"&":"")+encodeURIComponent(a)+"="+encodeURIComponent(t)),s.toString()},isPathWhitelisted=function(e,a){if(0===e.length)return!0;var t=new URL(a).pathname;return e.some(function(e){return t.match(e)})},stripIgnoredUrlParameters=function(e,a){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return a.every(function(a){return!a.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var a=e[0],t=e[1],c=new URL(a,self.location),s=createCacheKey(c,hashParamName,t,/\.\w{8}\./);return[c.toString(),s]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(a){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!a.has(t)){var c=new Request(t,{credentials:"same-origin"});return fetch(c).then(function(a){if(!a.ok)throw new Error("Request for "+t+" returned a response with status "+a.status);return cleanResponse(a).then(function(a){return e.put(t,a)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var a=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(t){return Promise.all(t.map(function(t){if(!a.has(t.url))return e.delete(t)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var a,t=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching);(a=urlsToCacheKeys.has(t))||(t=addDirectoryIndex(t,"index.html"),a=urlsToCacheKeys.has(t));0,a&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(t)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(a){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,a),fetch(e.request)}))}});
+"use strict";
+var precacheConfig = [
+    [
+      "/admin-dashboard-react-material/v1.0.2/index.html",
+      "c3315b0fe59ec93f1d9f1fe0739c7ea6",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/1.89388d3f.chunk.css",
+      "b4ca80ead6dbd7790bd9dfbfb0ae07f2",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/10.e9862a5a.chunk.css",
+      "a043577d594b8443660c767e263400cd",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/11.08df399b.chunk.css",
+      "0f5b09af13a8b6437d14ba52f22e2349",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/14.55f240e5.chunk.css",
+      "516395371186792a46cf25890c279d6d",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/15.ee862657.chunk.css",
+      "bd76b133377b557fb3f2635d7bf72951",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/17.1e88b062.chunk.css",
+      "7f6e789d68cff7f2d834461b7b1cdf35",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/23.90afa095.chunk.css",
+      "d9513b88c957b70064562f8bf6485954",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/5.82a17a21.chunk.css",
+      "4f2e84bd50a8523fe2876996246a21f9",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/6.ec66988b.chunk.css",
+      "a6479fe1ac4f965671480923edb40587",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/7.2b2ea20f.chunk.css",
+      "31130deb5ad904d46c84f1896c5c70c1",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/8.97b6738b.chunk.css",
+      "b0e84829f1489a224fb8f2976f1407bc",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/main.3f255066.chunk.css",
+      "a0ebf836059de45fd475b805465b0712",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/css/vendors.edfdd760.chunk.css",
+      "c24730bf8a33783752f8ab9c72048d7a",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/1.33050f36.chunk.js",
+      "2210e4f7be6abcbadcdc0640f46798bc",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/10.eee18a1a.chunk.js",
+      "fb68ab4a6ea1f6e5f178ca48a5a994a8",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/11.2c2bee7c.chunk.js",
+      "e5ac1c64b28ae47449b0b6d1e7805529",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/12.c8b98510.chunk.js",
+      "f5a2560b438a3cd760099fcd09d715a5",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/13.d9a9f293.chunk.js",
+      "0578988de3de4373078e346452319756",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/14.5589d18c.chunk.js",
+      "204d29087ae7ebef479cbe9433abfa8e",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/15.ee3845cf.chunk.js",
+      "d84f8084f64643ae5f828293f4d73d73",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/17.c404aeec.chunk.js",
+      "34e930feb2e1ed3b65edb08a172951a2",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/18.5b1b8b32.chunk.js",
+      "a9816ca35820b633674c993be587a0cd",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/19.4e327308.chunk.js",
+      "644ff158e70b658957b7c7efcb6c7476",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/2.cf5656e5.chunk.js",
+      "3dadc2bdd3922fc72a985c0a7799df4f",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/20.b5ff4660.chunk.js",
+      "d8244013570c3541652f4cd488a7db23",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/21.c10670b7.chunk.js",
+      "249ff6872d38e2999e0b00858a47fa38",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/22.ad8b5511.chunk.js",
+      "8a89ccfe4e2ec8dcce98190238d5ee4b",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/23.64ffafde.chunk.js",
+      "1acbb0033120aa8e2b49070d786f3b75",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/24.29ce54aa.chunk.js",
+      "098878ecfb475082b931576621ba6ada",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/25.b84b88d7.chunk.js",
+      "73179c88dfbf0645714989f1c6c44fcb",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/26.64b0d671.chunk.js",
+      "0c3b7939c68ee52eae0c222b0b1a6d74",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/27.44d99449.chunk.js",
+      "a73710235f89c1a60056362b45aae98e",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/28.427fbac8.chunk.js",
+      "c66d6d4f2797bbfc27deb67b33151fb1",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/29.efb72294.chunk.js",
+      "a4e22988b5d110244fce783dc3fc8d0f",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/3.c3ffd2cf.chunk.js",
+      "1b33b3d3a1aa1f1b2141ff12e2beea9c",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/30.15e2b739.chunk.js",
+      "9358a04ebea4311821a4acaba86f1318",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/4.bb9cb580.chunk.js",
+      "8c5f555a740d4a8dbd2bd3a7494c7a37",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/5.75a9171a.chunk.js",
+      "6306358c06f850a1d308d10e757737cf",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/6.35c2992c.chunk.js",
+      "fdd5165db9fe853aef8883758d432ab8",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/7.b4d98c9e.chunk.js",
+      "9db0ecdc191295a8e35ca73b961d8280",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/8.5a78adc8.chunk.js",
+      "39c591c18b4a610b9ec314099119ce61",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/9.1965f087.chunk.js",
+      "6792ac6697e2bf353fbb9d3079c2562f",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/main.dc355d82.chunk.js",
+      "75fa62deba5125c852b466a4bc665b8b",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/runtime~main.7c6bb4e5.js",
+      "fd692fdd876a054025a96ef96fef39ea",
+    ],
+    [
+      "/admin-dashboard-react-material/v1.0.2/static/js/vendors.0464e21f.chunk.js",
+      "b2ba45bdc4c16ffa0043611ae85a660b",
+    ],
+  ],
+  cacheName =
+    "sw-precache-v3-sw-precache-webpack-plugin-" +
+    (self.registration ? self.registration.scope : ""),
+  ignoreUrlParametersMatching = [/^utm_/],
+  addDirectoryIndex = function (e, a) {
+    var t = new URL(e);
+    return "/" === t.pathname.slice(-1) && (t.pathname += a), t.toString();
+  },
+  cleanResponse = function (e) {
+    return e.redirected
+      ? ("body" in e ? Promise.resolve(e.body) : e.blob()).then(function (a) {
+          return new Response(a, {
+            headers: e.headers,
+            status: e.status,
+            statusText: e.statusText,
+          });
+        })
+      : Promise.resolve(e);
+  },
+  createCacheKey = function (e, a, t, c) {
+    var s = new URL(e);
+    return (
+      (c && s.pathname.match(c)) ||
+        (s.search +=
+          (s.search ? "&" : "") +
+          encodeURIComponent(a) +
+          "=" +
+          encodeURIComponent(t)),
+      s.toString()
+    );
+  },
+  isPathWhitelisted = function (e, a) {
+    if (0 === e.length) return !0;
+    var t = new URL(a).pathname;
+    return e.some(function (e) {
+      return t.match(e);
+    });
+  },
+  stripIgnoredUrlParameters = function (e, a) {
+    var t = new URL(e);
+    return (
+      (t.hash = ""),
+      (t.search = t.search
+        .slice(1)
+        .split("&")
+        .map(function (e) {
+          return e.split("=");
+        })
+        .filter(function (e) {
+          return a.every(function (a) {
+            return !a.test(e[0]);
+          });
+        })
+        .map(function (e) {
+          return e.join("=");
+        })
+        .join("&")),
+      t.toString()
+    );
+  },
+  hashParamName = "_sw-precache",
+  urlsToCacheKeys = new Map(
+    precacheConfig.map(function (e) {
+      var a = e[0],
+        t = e[1],
+        c = new URL(a, self.location),
+        s = createCacheKey(c, hashParamName, t, /\.\w{8}\./);
+      return [c.toString(), s];
+    })
+  );
+function setOfCachedUrls(e) {
+  return e
+    .keys()
+    .then(function (e) {
+      return e.map(function (e) {
+        return e.url;
+      });
+    })
+    .then(function (e) {
+      return new Set(e);
+    });
+}
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(function (e) {
+        return setOfCachedUrls(e).then(function (a) {
+          return Promise.all(
+            Array.from(urlsToCacheKeys.values()).map(function (t) {
+              if (!a.has(t)) {
+                var c = new Request(t, { credentials: "same-origin" });
+                return fetch(c).then(function (a) {
+                  if (!a.ok)
+                    throw new Error(
+                      "Request for " +
+                        t +
+                        " returned a response with status " +
+                        a.status
+                    );
+                  return cleanResponse(a).then(function (a) {
+                    return e.put(t, a);
+                  });
+                });
+              }
+            })
+          );
+        });
+      })
+      .then(function () {
+        return self.skipWaiting();
+      })
+  );
+}),
+  self.addEventListener("activate", function (e) {
+    var a = new Set(urlsToCacheKeys.values());
+    e.waitUntil(
+      caches
+        .open(cacheName)
+        .then(function (e) {
+          return e.keys().then(function (t) {
+            return Promise.all(
+              t.map(function (t) {
+                if (!a.has(t.url)) return e.delete(t);
+              })
+            );
+          });
+        })
+        .then(function () {
+          return self.clients.claim();
+        })
+    );
+  }),
+  self.addEventListener("fetch", function (e) {
+    if ("GET" === e.request.method) {
+      var a,
+        t = stripIgnoredUrlParameters(
+          e.request.url,
+          ignoreUrlParametersMatching
+        );
+      (a = urlsToCacheKeys.has(t)) ||
+        ((t = addDirectoryIndex(t, "index.html")),
+        (a = urlsToCacheKeys.has(t)));
+      0,
+        a &&
+          e.respondWith(
+            caches
+              .open(cacheName)
+              .then(function (e) {
+                return e.match(urlsToCacheKeys.get(t)).then(function (e) {
+                  if (e) return e;
+                  throw Error(
+                    "The cached response that was expected is missing."
+                  );
+                });
+              })
+              .catch(function (a) {
+                return (
+                  console.warn(
+                    'Couldn\'t serve response for "%s" from cache: %O',
+                    e.request.url,
+                    a
+                  ),
+                  fetch(e.request)
+                );
+              })
+          );
+    }
+  });
